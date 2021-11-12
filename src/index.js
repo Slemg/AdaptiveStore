0ad7742591bad20875321565ac45375384573d99
satikpro <saturnpro73@gmail.com>
2021-11-12 03:14:06 +0200

    fix-mobile-slider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 $('#main-content').slick({
   dots: true,
-  slidesToShow: 2,
-  slidesToScroll: 2,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-   
-  ]
+  slidesToShow: 1,
+  slidesToScroll: 1,
+
+
 });
 $('#main-content-pc').slick({
   dots: true,
